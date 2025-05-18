97b59625af0c1fc3c28da9c48150936fd5d8f8f9
Arsenty Halimovsky <ghost12@yandex.ru>
2025-05-18 07:51:56 +0300

    Instantiate the project

---
+++ ThinkingInSwiftUI/ThinkingInSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Арсентий Халимовский on 18.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThinkingInSwiftUI/ThinkingInSwiftUI/ThinkingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingInSwiftUIApp.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Арсентий Халимовский on 18.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
